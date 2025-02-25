38a6bf9d7d053b8b48763d94c2f06e4ce7482587
Fer2345Romero <fernandaromeroclaros@gmail.com>
2025-02-24 22:59:03 -0600

    CRUD - Update and Delete

--- Controllers/AlumnosController.cs
+++ Controllers/AlumnosController.cs
@@ -3,6 +3,7 @@ using CRUD_MVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace CRUD_MVC.Controllers
 {
@@ -20,26 +21,60 @@ namespace CRUD_MVC.Controllers
             return View(alumnos);
         }
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult UpSert(int id)
         {
-            Alumnos alumno = new();
-            return View(alumno);
+            if(id == 0)
+            {
+                //Registro nuevo
+                Alumnos alumno = new();
+                return View(alumno);
+            }
+            else
+            {
+                //Registro existente
+                Alumnos alumno = _dbConnetion.Alumnos.FirstOrDefault(row => row.AlumnoId == id) ?? new();
+                return View(alumno);
+
+            }
+           
         }
 
         [HttpPost]
 
-        public IActionResult Create(Alumnos model)
+        public IActionResult UpSert(Alumnos model)
         {
             //remover propiedad --No se esta ocupando 
             ModelState.Remove("NombreCompleto");
-            if (ModelState.IsValid)
+            if (model.AlumnoId == 0)
             {
-                _dbConnetion.Alumnos.Add(model);
-                _dbConnetion.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    _dbConnetion.Alumnos.Add(model);
+                    _dbConnetion.SaveChanges();
+                    return RedirectToAction("Index");
+                }       
             }
+            else 
+            {
+                if(ModelState.IsValid)
+                {
+                    _dbConnetion.Alumnos.Update(model);
+                    _dbConnetion.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }  
             //Retorna a unas accion especifica en el controlador
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Alumnos alumno = _dbConnetion.Alumnos.FirstOrDefault(row => row.AlumnoId == id)?? new();
+            alumno.IsActive = false;
+            _dbConnetion.Alumnos.Update(alumno);
+            _dbConnetion.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
